72a9657e7f74c4fa4a82faa0fb79df82a8beb144
Chase Miller <millerchase676@gmail.com>
2026-03-10 17:15:22 -0400

    Nearly complete, just need to implement image functionality.

--- data-service.js
+++ data-service.js
@@ -7,7 +7,7 @@ async function initialize() {
     try {
         return await Promise.all([
             fs.readFile("./data/employees.json", "utf8").then(data => employees = JSON.parse(data)),
-            fs.readFile("./data/departments.json", "utf8").then(data_1 => departments = JSON.parse(data_1))
+            fs.readFile("./data/departments.json", "utf8").then(data => departments = JSON.parse(data))
         ]);
     } catch (err) {
         console.error("Error initializing data:", err);
@@ -18,49 +18,35 @@ async function initialize() {
 module.exports = {
     getAllEmployees: async () => {
         await initialize();
-        if (employees.length === 0) {
-            throw new Error("no results returned");
-        }
         return employees;
     },
     getEmployeesByStatus: async (status) => {
         await initialize();
-        const filteredEmployees = employees.filter(employee => employee.status === status);
-        if (filteredEmployees.length === 0) {
-            throw new Error("no results returned");
-        }
+        const filteredEmployees = employees.filter(employee => employee.status == status);
         return filteredEmployees;
     },
     getEmployeesByDepartment: async (department) => {
         await initialize();
-        const filteredEmployees = employees.filter(employee => employee.department === department);
-        if (filteredEmployees.length === 0) {
-            throw new Error("no results returned");
-        }
+        const filteredEmployees = employees.filter(employee => employee.department == department);
         return filteredEmployees;
     },
     getEmployeesByManager: async (manager) => {
         await initialize();
-        const filteredEmployees = employees.filter(employee => employee.employeeManagerNum === parseInt(manager));
-        if (filteredEmployees.length === 0) {
-            throw new Error("no results returned");
-        }
+        const filteredEmployees = employees.filter(employee => employee.employeeManagerNum == parseInt(manager));
         return filteredEmployees;
     },
     getManagers: async () => {
         await initialize();
         const managers = employees.filter(employee => employee.isManager);
-        if (managers.length === 0) {
-            throw new Error("no results returned");
-        }
         return managers;
     },
     getDepartments: async () => {
         await initialize();
-        if (departments.length === 0) {
-            throw new Error("no results returned");
-        }
         return departments;
+    },
+    getDepartmentById: async (id) => {
+	await initialize();
+	return;
     },
     addEmployee: async (employeeData) => {
         try {
@@ -78,6 +64,3 @@ module.exports = {
         }
     }
 };
-
-
-
